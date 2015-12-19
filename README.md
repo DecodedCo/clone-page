dcb8385e001d4578f9a25fa7197eba4f297a7e5e
Alex <alex@decoded.co>
2015-12-19 14:01:22 +0000

    first push

---
+++ README.md
@@ -0,0 +1,5 @@
+## Useful for cloning webpages
+
+* Grabs the html of the current webpage
+* Injects the HTML `base` tag into the `<head>` so that links/css/image/javascript come from original source
+* Useful for demonstrating ease of creating phishing sites
---
+++ getPagesSource.js
@@ -0,0 +1,39 @@
+// @author Rob W <http://stackoverflow.com/users/938089/rob-w>
+// Demo: var serialized_html = DOMtoString(document);
+
+function DOMtoString(document_root) {
+    var base = document.createElement('base');
+    base.href = window.location.href;
+    console.log(base)
+    document_root.getElementsByTagName('head')[0].appendChild(base);
+    var html = '',
+        node = document_root.firstChild;
+    while (node) {
+        switch (node.nodeType) {
+        case Node.ELEMENT_NODE:
+            html += node.outerHTML;
+            break;
+        case Node.TEXT_NODE:
+            html += node.nodeValue;
+            break;
+        case Node.CDATA_SECTION_NODE:
+            html += '<![CDATA[' + node.nodeValue + ']]>';
+            break;
+        case Node.COMMENT_NODE:
+            html += '<!--' + node.nodeValue + '-->';
+            break;
+        case Node.DOCUMENT_TYPE_NODE:
+            // (X)HTML documents are identified by public identifiers
+            html += "<!DOCTYPE " + node.name + (node.publicId ? ' PUBLIC "' + node.publicId + '"' : '') + (!node.publicId && node.systemId ? ' SYSTEM' : '') + (node.systemId ? ' "' + node.systemId + '"' : '') + '>\n';
+            break;
+        }
+        node = node.nextSibling;
+    }
+
+    return html;
+}
+
+chrome.runtime.sendMessage({
+    action: "getSource",
+    source: DOMtoString(document)
+});
